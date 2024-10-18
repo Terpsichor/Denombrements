f106419f4a958710f5ea9932d0b3e24cfe15bb0e
Alexis <alexis.muller.pro@gmail.com>
2024-10-18 13:30:05 +0200

    J'ai pas toute les instructioons boss...

--- Denombrements/Program.cs
+++ Denombrements/Program.cs
@@ -34,6 +34,22 @@ namespace Denombrements
         }
 
         /// <summary>
+        /// Multiplication d'une suite d'entier, d'une valeur à une autre.
+        /// </summary>
+        /// <param name="val1">La première valeur</param>
+        /// <param name="val2">La seconde valeur</param>
+        /// <returns></returns>
+        static long Multiplication(int val1, int val2)
+        {
+            long resultat = 1;
+            for (int k = val1; k <= val2; k++)
+            {
+                resultat *= k;
+            }
+            return resultat;
+        }
+
+        /// <summary>
         /// Programme de calcul de Permutation, Arrangement et Combinaison
         /// </summary>
         /// <param name="args"></param>
@@ -54,24 +70,24 @@ namespace Denombrements
                 {
                     case 1:
                         nombre = Saisie("nombre total d'éléments à gérer = ");
-                        // calcul de r
-                        resultat = 1;
-                        for (int k = 1; k <= nombre; k++)
+                        // calcul de resultat
+                        resultat = Multiplication(1, nombre);
+                        /*for (int k = 1; k <= nombre; k++)
                         {
                             resultat *= k;
-                        }
+                        } */
                         Console.WriteLine(nombre + "! = " + resultat);
                         break;
 
                     case 2:
                         total = Saisie("nombre total d'éléments à gérer = ");
                         nombre = Saisie("nombre d'éléments dans le sous ensemble = ");
-                        // calcul de r
-                        resultat = 1;
-                        for (int k = (total - nombre + 1); k <= total; k++)
+                        // calcul de resultat
+                        resultat = Multiplication((total - nombre + 1), total);
+                        /*for (int k = (total - nombre + 1); k <= total; k++)
                         {
                             resultat *= k;
-                        }
+                        } */
                         Console.WriteLine("A(" + total + "/" + nombre + ") = " + resultat);
                         break;
 
@@ -79,17 +95,17 @@ namespace Denombrements
                         total = Saisie("nombre total d'éléments à gérer = ");
                         nombre = Saisie("nombre d'éléments dans le sous ensemble = ");
                         // calcul de r1
-                        resultat1 = 1;
-                        for (int k = (total - nombre + 1); k <= total; k++)
+                        resultat1 = Multiplication((total - nombre + 1), total);
+                        /*for (int k = (total - nombre + 1); k <= total; k++)
                         {
                             resultat1 *= k;
-                        }
+                        } */
                         // calcul de r2
-                        resultat2 = 1;
-                        for (int k = 1; k <= nombre; k++)
+                        resultat2 = Multiplication(1, nombre);
+                        /*for (int k = 1; k <= nombre; k++)
                         {
                             resultat2 *= k;
-                        }
+                        } */
                         Console.WriteLine("C(" + total + "/" + nombre + ") = " + (resultat1 / resultat2));
                         break;
                 }
